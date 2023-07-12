617ef838a98f5df91eba75cfd788cfdbd71a96d0
skrel <krel.svyatoslav@gmail.com>
2023-07-12 09:52:37 -0700

    20230712

--- components/dboperations.js
+++ components/dboperations.js
@@ -1,18 +1,165 @@
-var  config = require('./dbconfig')
-const  sql = require('mssql')
+var config = require('./dbconfig')
+const sql = require('mssql')
 
-// query all data
-async  function  getDataFromSQL() {
-  try {
-    let  pool = await  sql.connect(config)
-    let  products = await  pool.request().query("SELECT * from testData")
-    return  products.recordsets
-  }
-  catch (error) {
-    console.log(error)
-  }
+// for testing
+// query all data from test table
+async function getDataFromSQL() {
+    try {
+        let pool = await sql.connect(config)
+        let products = await pool.request().query("SELECT * from testData")
+        return products.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+// insert data into sql
+async function insertCCDLastUpdate(ccd_last_update) {
+    // wrap into single quotes
+    let ccdlastUpdate = '\'' + ccd_last_update + '\''
+    let queryString = 'INSERT INTO ccd_sch_last_update (CCD_LAST_UPDATE) VALUES (' + ccdlastUpdate + ')'
+    try {
+        let pool = await sql.connect(config)
+        await pool.request().query(queryString)
+        console.log('ccd record saved')
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+async function insertReonomyLastUpdate(reonomy_last_update) {
+    // wrap into single quotes
+    let reonomylastUpdate = '\'' + reonomy_last_update + '\''
+    let queryString = 'INSERT INTO ccd_sch_last_update (CCD_LAST_UPDATE) VALUES (' + reonomylastUpdate + ')'
+    try {
+        let pool = await sql.connect(config)
+        await pool.request().query(queryString)
+        console.log('reonomy record saved')
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
+
+
+
+// query three tables costar, reonomy, and ccd
+
+// query ccd data
+async function getCcdData() {
+    try {
+        // connect
+        let pool = await sql.connect(config)
+
+        // get ccd
+        let ccdAzure = await pool.request().query("SELECT * from ccd_sch")
+
+        // return
+        return ccdAzure.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+// query reonomy data
+async function getReonomyData() {
+    try {
+        // connect
+        let pool = await sql.connect(config)
+
+        // get reonomy
+        let reonomyAzure = await pool.request().query("SELECT * from reonomy")
+
+        // return
+        return reonomyAzure.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+// query costar data
+async function getCostarData() {
+    try {
+        // connect
+        let pool = await sql.connect(config)
+
+        // get costar
+        let costarAzure = await pool.request().query("SELECT * from costar")
+
+        // return
+        return costarAzure.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+// query costar data
+async function getCddEnrollmentData() {
+    try {
+        // connect
+        let pool = await sql.connect(config)
+
+        // get ccd enrollment
+        let ccdEnrollmentAzure = await pool.request().query("SELECT * from ccd_enrollment")
+
+        // return
+        return ccdEnrollmentAzure.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
+
+// need to query last update for all raw data sources
+// query ccd last update
+async function getCddLastUpdate() {
+    try {
+        let pool = await sql.connect(config)
+        let ccdLastUpdateAzure = await pool.request().query("SELECT * from ccd_sch_last_update ORDER BY CCD_LAST_UPDATE DESC")
+        return ccdLastUpdateAzure.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+// query reonomy last update
+async function getReonomyLastUpdate() {
+    try {
+        let pool = await sql.connect(config)
+        let reonomyLastUpdateAzure = await pool.request().query("SELECT * from reonomy_last_update ORDER BY REONOMY_LAST_UPDATE DESC")
+        return reonomyLastUpdateAzure.recordsets
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
     getDataFromSQL: getDataFromSQL,
+    getCcdData: getCcdData,
+    getReonomyData: getReonomyData,
+    getCostarData: getCostarData,
+    getCddEnrollmentData: getCddEnrollmentData,
+
+    getCddLastUpdate: getCddLastUpdate,
+    getReonomyLastUpdate: getReonomyLastUpdate,
+
+    insertCCDLastUpdate: insertCCDLastUpdate,
+    insertReonomyLastUpdate: insertReonomyLastUpdate
 }
--- server.js
+++ server.js
@@ -21,56 +21,252 @@ const request = require('request')
 const azure = require('azure-storage')
 const fileUpload = require("express-fileupload")
 const getStream = require('into-stream')
-
 var Db = require('./components/dboperations')
-var arrToCsv = require('./components/arrayToCsv')
-var downloadFile = require('./components/downloadBlob')
 const fs = require('fs')
-
+let converter = require('json-2-csv')
 // View Engine Setup
 const path = require('path')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
 const blobService = azure.createBlobService('DefaultEndpointsProtocol=https;AccountName=cscdwfileupload;AccountKey=RlreQ0k6PR39bPVw5aEsjvk3LnIFBQXfmixD9sMBlYL+I2LcMlMveb/L6YT5a7gg6+FqMFsYMsyP+AStNiTApQ==;EndpointSuffix=core.windows.net')
-
 app.use(express.static('page'))
 
-app.get('/', function(req, res){
+
+
+// declare global variables
+var reonLastUpd
+
+
+// when the page first load or reload
+// TODO: add LOADING spin
+app.get('/', async function (req, res) {
+
+
+    // support last update functionality
+    // query sql to find out when the data was updated
+    // create global variables
+    let ccdDataArray = []
+    let reonomyDataArray = []
+    let costarDataArray = []
+    let ccdEnrDataArray = []
+
+    let ccdLastUpdateOLD = []
+    let reonomyLastUpdateOLD = []
+
+    // use async-await to get sql data out of the request
+    // get ccd data
+    await Db.getCcdData()
+        .then((ccdData) => {
+            ccdDataArray = ccdData[0]
+        })
+    await Db.getCddLastUpdate()
+        .then((ccdLastUpdate) => {
+            ccdLastUpdateOLD = ccdLastUpdate[0]
+        })
+
+// get reonomy data
+    await Db.getReonomyData()
+        .then((reonomyData) => {
+            reonomyDataArray = reonomyData[0]
+        })
+        await Db.getReonomyLastUpdate()
+        .then((reonomyLastUpdate) => {
+            reonomyLastUpdateOLD = reonomyLastUpdate[0]
+        })
+
+        // get costar data
+    await Db.getCostarData()
+        .then((costarData) => {
+            costarDataArray = costarData[0]
+        })
+
+        // get ccd enr data
+    await Db.getCddEnrollmentData()
+        .then((ccdEnrData) => {
+            ccdEnrDataArray = ccdEnrData[0]
+        })
+
+    let ccdLastUpdate = ccdDataArray[0].LAST_UPDATE
+    let reonomyLastUpdate = reonomyDataArray[0].LAST_UPDATE
+    let costarLastUpdate = costarDataArray[0].LAST_UPDATE
+    let ccdEnrLastUpdate = ccdEnrDataArray[0].LAST_UPDATE
+
+    // old and new dates ccd
+    let ccdNewDate = new Date(ccdDataArray[0].LAST_UPDATE)
+    let ccdOldDate = new Date(ccdLastUpdateOLD[0].CCD_LAST_UPDATE)
+
+    // old and new dates reonomy
+    let reonomyNewDate = new Date(ccdDataArray[0].LAST_UPDATE)
+    let reonomyOldDate = new Date(ccdLastUpdateOLD[0].CCD_LAST_UPDATE)
+
+// lldb refresh kick in when raw data has been updated
+    if(ccdNewDate.toString() !== ccdOldDate.toString() 
+        || reonomyNewDate.toString() !== reonomyOldDate.toString()) {
+
+        console.log('fire update')
+
+        // CCD convert dates and create a record
+        if(ccdNewDate.toString() !== ccdOldDate.toString()) {
+            let time = ccdNewDate.toISOString().split('T')[1]
+            let insertIntoSQLccd = ccdNewDate.toISOString().split('T')[0] + ' ' + time.substring(0, time.length - 1)
+            Db.insertCCDLastUpdate(insertIntoSQLccd.toString())
+        }
+
+        // Reonomy convert dates and create a record
+        if(reonomyNewDate.toString() !== reonomyOldDate.toString()) {
+            let time = reonomyNewDate.toISOString().split('T')[1]
+            let insertIntoSQLreonomy = reonomyNewDate.toISOString().split('T')[0] + ' ' + time.substring(0, time.length - 1)
+            Db.insertReonomyLastUpdate(insertIntoSQLreonomy.toString())
+        }
+        
+
+        // assemble lldb and send it to sql
+        var lldbArray = [];
+
+        for (var ccd_i = 0; ccd_i < ccdDataArray.length; ccd_i++) {
+            var ccd_obj = ccdDataArray[ccd_i]
+            // console.log('------->', ccd_obj.STATENAME)
+            var lldbItem = {
+                Owner_ID: null,
+                Tenant_Name: ccd_obj.SCH_NAME,
+                STUDENT_COUNT: null,
+                ESTIMATED_REVENUE_PER_STUDENT: null,
+                PROPERTY_ADDRESS_STREET: null,
+                PROPERTY_ADDRESS_CITY: null,
+                PROPERTY_ADDRESS_STATE: null,
+                PROPERTY_ADDRESS_ZIP_CODE: null,
+                GROSS_BUILDING_AREA: null,
+                MSA: null,
+                MARKET_CAP_RATE: null,
+                MARKET_SALE_PRICE_PER_SF: null,
+                VALUATION_METHOD: null,
+                CSC_SALE_PRICE_PER_SF_DISCOUNT: null,
+                CSC_CAP_RATE_PREMIUM: null,
+                RENT_TO_REVENUE_FLAT_ASSUMPTION: null,
+                Account_ID: null,
+                Industry: null,
+                LLDB_Date_Added: null,
+                LLDB_Last_Updated: null,
+                Currently_Assigned_Broker: null,
+                PRIMARY_CONTACT_FIRST_NAME: null,
+                PRIMARY_CONTACT_LAST_NAME: null,
+                PRIMARY_CONTACT_TITLE: null,
+                PRIMARY_CONTACT_PHONE: null,
+                PRIMARY_CONTACT_EMAIL: null,
+                PRIMARY_CONTACT_ADDRESS: null,
+                PRIMARY_CONTACT_CITY: null,
+                PRIMARY_CONTACT_STATE: null,
+                PRIMARY_CONTACT_ZIP_CODE: null,
+                CONTACT_RECORD_TYPE_ID: null,
+                TENANT_RECORD_TYPE_ID: null,
+                ACCOUNT_RECORD_TYPE_ID: null,
+                REONOMY_ID: ccd_obj.reonomy_id,
+                CCD_ID: ccd_obj.SCHID
+            }
+    
+            // bring ccd enr data
+            // TODO: ccd_obj.SCHID is a number, but ccs_enr_obj.SCHID is a string, so either one has to be converted
+            // adjust data types in sql to correct this
+            for (var ccs_enr_i = 0; ccs_enr_i < ccdEnrDataArray.length; ccs_enr_i++) {
+                var ccs_enr_obj = ccdEnrDataArray[ccs_enr_i]
+                if (ccd_obj.SCHID.toString() == ccs_enr_obj.SCHID) {
+                    lldbItem.STUDENT_COUNT = ccs_enr_obj.STUDENT_COUNT
+                }
+            }
+    
+            // bring costar data
+            // Important!!! - convert data toLowerCase() before comparison
+            for (var costar_i = 0; costar_i < costarDataArray.length; costar_i++) {
+                var costar_obj = costarDataArray[costar_i]
+                if (ccd_obj.LCITY.toLowerCase() == costar_obj.CITY.toLowerCase() && ccd_obj.LSTATE.toLowerCase() == costar_obj.STATE.toLowerCase()) {
+                    lldbItem.MARKET_CAP_RATE = costar_obj.Overall_Market_Cap_Rate
+                    lldbItem.MARKET_SALE_PRICE_PER_SF = costar_obj.Market_Sale_Price_per_SF
+                }
+            }
+    
+            // bring reonomy data
+            for (var reonomy_i = 0; reonomy_i < reonomyDataArray.length; reonomy_i++) {
+                var reonomy_obj = reonomyDataArray[reonomy_i]
+                if (ccd_obj.reonomy_id == reonomy_obj.reonomy_id) {
+                    lldbItem.PROPERTY_ADDRESS_STREET = reonomy_obj.address_line_1
+                    lldbItem.PROPERTY_ADDRESS_CITY = reonomy_obj.address_city
+                    lldbItem.PROPERTY_ADDRESS_STATE = reonomy_obj.address_state
+                    lldbItem.PROPERTY_ADDRESS_ZIP_CODE = reonomy_obj.address_postal_code
+                    lldbItem.GROSS_BUILDING_AREA = reonomy_obj.gross_building_area
+                    lldbItem.PRIMARY_CONTACT_FIRST_NAME = reonomy_obj.contact_name
+                    lldbItem.PRIMARY_CONTACT_LAST_NAME = reonomy_obj.contact_name
+                    lldbItem.PRIMARY_CONTACT_TITLE = reonomy_obj.contact_title
+                    lldbItem.PRIMARY_CONTACT_PHONE = reonomy_obj.contact_phone_1
+                    lldbItem.PRIMARY_CONTACT_EMAIL = reonomy_obj.contact_email_1
+                    lldbItem.PRIMARY_CONTACT_ADDRESS = reonomy_obj.contact_address_1_line_1
+                    lldbItem.PRIMARY_CONTACT_CITY = reonomy_obj.contact_address_1_city
+                    lldbItem.PRIMARY_CONTACT_STATE = reonomy_obj.contact_address_1_state
+                    lldbItem.PRIMARY_CONTACT_ZIP_CODE = reonomy_obj.contact_address_1_postal_code
+                }
+            }
+    
+            lldbArray.push(lldbItem)
+            
+        }
+
+        // Usefull: check the array before export 
+        console.log('-----> my lldb table with ccd+enrollment+costar+reonomy data  ---->', lldbArray.length)
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    // return this
     res.render('index', {
         successMessageCCD: '',
         successMessageReonomy: '',
         successMessageCCDEnrollment: '',
+        successMessageCostar: '',
         errorMessage: '',
-        successFileSave: '' 
+        successFileSave: '',
+        ccdLastUpdateToDisplay: ccdLastUpdate,
+        reonomyLastUpdateToDisplay: reonomyLastUpdate,
+        costarLastUpdateToDisplay: costarLastUpdate,
+        ccdEnrLastUpdateToDisplay: ccdEnrLastUpdate
     })
 })
 
+
+// File upload
+// TODO: make one POST per form, rater than all in one
 app.post('/postfile',
     fileUpload({ createParentPath: true }),
     (req, res) => {
-        
-        const files = req.files
-        
 
-        if(!req.files) {
-            res.render("index", { 
+        const files = req.files
+
+        if (!req.files) {
+            res.render("index", {
                 successMessageCCD: '',
                 successMessageReonomy: '',
                 successMessageCCDEnrollment: '',
+                successMessageCostar: '',
                 successFileSave: '',
                 errorMessage: "ERROR: No files attached."
             })
         } else {
             const rawBlobName = files.file.name
-        
+
             let containerName
             let stream
             let streamLength
             let successMessage
             let errorMessage
 
-            if(rawBlobName == 'ccd_sch.csv') {
+            if (rawBlobName == 'ccd_sch.csv') {
                 console.log('===================== I AM CCD DATA')
                 containerName = 'ccd'
                 blobName = 'ccd/' + rawBlobName
@@ -78,7 +274,7 @@ app.post('/postfile',
                 stream = getStream(files.file.data)
                 successMessage = '... ccd processing'
             }
-            if(rawBlobName == 'reonomy.csv') {
+            if (rawBlobName == 'reonomy.csv') {
                 console.log('===================== I AM REONOMY DATA')
                 containerName = 'reonomy'
                 blobName = 'reonomy/' + rawBlobName
@@ -86,7 +282,7 @@ app.post('/postfile',
                 stream = getStream(files.file.data)
                 successMessage = '... reonomy processing'
             }
-            if(rawBlobName == 'ccd_enrollment.csv') {
+            if (rawBlobName == 'ccd-enrollment.csv') {
                 console.log('===================== I AM CCD ENROLLMENT DATA')
                 containerName = 'ccd-enrollment'
                 blobName = 'ccd-enrollment/' + rawBlobName
@@ -94,47 +290,88 @@ app.post('/postfile',
                 stream = getStream(files.file.data)
                 successMessage = '... ccd-enrollment processing'
             }
-    
+            if (rawBlobName == 'costar.csv') {
+                console.log('===================== I AM COSTAR DATA')
+                containerName = 'costar'
+                blobName = 'costar/' + rawBlobName
+                streamLength = files.file.size
+                stream = getStream(files.file.data)
+                successMessage = '... costar processing'
+            }
+
             console.log(containerName)
             console.log(rawBlobName)
             console.log(blobName)
             console.log(successMessage)
-    
+
             blobService.createBlockBlobFromStream(
                 containerName,
                 blobName,
                 stream,
                 streamLength,
                 (err) => {
-                    if(!err) {
-                        if(rawBlobName == 'ccd_sch.csv') {
-                            res.render("index", { 
+                    if (!err) {
+                        if (rawBlobName == 'ccd_sch.csv') {
+                            res.render("index", {
                                 successMessageCCD: successMessage,
                                 successMessageReonomy: '',
                                 successMessageCCDEnrollment: '',
+                                successMessageCostar: '',
                                 successFileSave: '',
-                                errorMessage: ''
+                                errorMessage: '',
+                                // last updates
+                                ccdLastUpdateToDisplay: 'processing',
+                                ccdEnrLastUpdateToDisplay: 'processing',
+                                reonomyLastUpdateToDisplay: 'processing',
+                                costarLastUpdateToDisplay: 'processing'
                             })
                         }
-                        if(rawBlobName == 'reonomy.csv') {
-                            res.render("index", { 
+                        if (rawBlobName == 'reonomy.csv') {
+                            res.render("index", {
                                 successMessageCCD: '',
                                 successMessageReonomy: successMessage,
                                 successMessageCCDEnrollment: '',
+                                successMessageCostar: '',
                                 successFileSave: '',
-                                errorMessage: ''
+                                errorMessage: '',
+                                // last updates
+                                ccdLastUpdateToDisplay: 'processing',
+                                ccdEnrLastUpdateToDisplay: 'processing',
+                                reonomyLastUpdateToDisplay: 'processing',
+                                costarLastUpdateToDisplay: 'processing'
                             })
                         }
-                        if(rawBlobName == 'ccd-enrollment.csv') {
-                            res.render("index", { 
+                        if (rawBlobName == 'ccd-enrollment.csv') {
+                            res.render("index", {
                                 successMessageCCD: '',
                                 successMessageReonomy: '',
                                 successMessageCCDEnrollment: successMessage,
+                                successMessageCostar: '',
                                 successFileSave: '',
-                                errorMessage: ''
+                                errorMessage: '',
+                                // last updates
+                                ccdLastUpdateToDisplay: 'processing',
+                                ccdEnrLastUpdateToDisplay: 'processing',
+                                reonomyLastUpdateToDisplay: 'processing',
+                                costarLastUpdateToDisplay: 'processing'
                             })
                         }
-                        
+                        if (rawBlobName == 'costar.csv') {
+                            res.render("index", {
+                                successMessageCCD: '',
+                                successMessageReonomy: '',
+                                successMessageCCDEnrollment: '',
+                                successMessageCostar: successMessage,
+                                successFileSave: '',
+                                errorMessage: '',
+                                // last updates
+                                ccdLastUpdateToDisplay: 'processing',
+                                ccdEnrLastUpdateToDisplay: 'processing',
+                                reonomyLastUpdateToDisplay: 'processing',
+                                costarLastUpdateToDisplay: 'processing'
+                            })
+                        }
+
                     } else {
                         res.render("index", { errorMessage: "ERROR: Something went wrong." })
                     }
@@ -142,42 +379,40 @@ app.post('/postfile',
             )
         }
 
-        
+
     })
 
-    app.get('/datafromsql', function(req, res){
 
-        let dataToDownload = [["Col1", "Col2", "Col3"]]
-        Db.getDataFromSQL()
+// DELETE
+// shows how to save data from sql into csv
+// this one is for testing only and can be deleted
+app.get('/datafromsql', function (req, res) {
+    Db.getDataFromSQL()
         .then((data) => {
-            // console.log('sqlData = ', data[0])
             var myData = data[0]
-            for(var i = 0; i < myData.length; i++) {
-                var object = myData[i]
-                // console.log('object = ', object)
-                const item = [
-                    object.Col1,
-                    object.Col2,
-                    object.Col3
-                ]
-                dataToDownload.push(item)
-            }
-            // console.log('array = ', dataToDownload)
-            let csv = arrToCsv.arrayToCsv(dataToDownload)
-            downloadFile.downloadBlob(csv, '/Users/skrel/Downloads/data_warehouse_export.csv')
+            converter.json2csv(myData)
+                .then((csv) => {
+                    console.log('csv = ', csv)
+                    //this statement tells the browser what type of data is supposed to download and force it to download
+                    res.writeHead(200, {
+                        'Content-Type': 'text/csv',
+                        'Content-Disposition': 'attachment; filename=data_warehouse_export.csv'
+                    });
+                    // whereas this part is in charge of telling what data should be parsed and be downloaded
+                    res.end(csv, "binary");
+                })
         })
-        
+})
+// DELETE
 
-        
 
-        res.render('index', {
-            successMessageCCD: '',
-            successMessageReonomy: '',
-            successMessageCCDEnrollment: '',
-            errorMessage: '',
-            successFileSave: 'The file has been saved!'
-        })
-    })
+// get lldb
+// used to query SQL and assemble lldb
+app.get('/getlldb', function (req, res) {
+
+    console.log('get lldb button pressed')
+
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
